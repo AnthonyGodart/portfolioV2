7eb3327e019509ba2befdfdbfc7dba832e4ec422
Anthony GODART <anthonygodart@macbook-pro-de-anthony.home>
2024-02-29 10:21:34 +0100

    added animations while opening paragraphs

--- utils/infosDisplayer.js
+++ utils/infosDisplayer.js
@@ -1,15 +1,55 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const container = document.getElementById('profile-infos')
+    const container = document.getElementById('profile-infos');
     const displayer = document.getElementById('displayer');
     const coordinates = document.getElementById('coordinates');
-    coordinates.style.visibility = 'hidden';
+    coordinates.style.height = '0';
+    coordinates.style.overflow ='hidden';
+    let isOpen = false;
+    let isAnimating = false; 
 
     displayer.addEventListener('mouseenter', function() {
-        coordinates.style.visibility = 'visible';
+        
+        if (!isOpen && !isAnimating) {
+            isAnimating = true; 
+            isOpen = true;
+            let height = 0;
+            const maxHeight = coordinates.scrollHeight;
+            const animationDuration = 500; 
+            const intervalDuration = 10; 
+            
+            const intervalId = setInterval(function() {
+                
+                height += maxHeight / (animationDuration / intervalDuration);
+                coordinates.style.height = (height + 3) + 'px';
+                                
+                if (height >= maxHeight) {
+                    clearInterval(intervalId);
+                    isAnimating = false; 
+                }
+            }, intervalDuration);
+        }
     });
 
     container.addEventListener('mouseleave', function() {
-        coordinates.style.visibility = 'hidden';
-    })
-
+        
+        if (isOpen && !isAnimating) {
+            isAnimating = true;
+            isOpen = false;
+            let height = coordinates.scrollHeight;
+            const animationDuration = 500;
+            const intervalDuration = 10;
+            
+            const intervalId = setInterval(function() {
+                
+                height -= coordinates.scrollHeight / (animationDuration / intervalDuration);
+                coordinates.style.height = height + 'px';
+                                
+                if (height <= 0) {
+                    clearInterval(intervalId);
+                    coordinates.style.height = '0';
+                    isAnimating = false;
+                }
+            }, intervalDuration);
+        }
+    });
 });
