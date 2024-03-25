6c5602d03923ca5ce9eef198851e7a886ad91114
Anthony GODART <anthonygodart@macbook-pro-de-anthony.home>
2024-03-25 10:01:57 +0100

    added and completed projects

--- utils/projects.js
+++ utils/projects.js
@@ -5,8 +5,8 @@ const projects = [
             en: "Numeric to string converter"
         },
         description: {
-            fr: "Description du projet, en cours de rédaction...",
-            en: "Description of project, in progress..."
+            fr: "Réalisation d'une macro pour Google Sheets, permettrant de convertir une valeur numérique en toutes lettres ( français uniquement ), notamment à destination de documents comptables.",
+            en: "Creation of a macro for Google Sheets, allowing you to convert a numerical value into words ( french only ), particularly for accounting documents."
         },
         linkText: {
             fr: "Lien vers le repo",
@@ -20,8 +20,8 @@ const projects = [
             en: "Portfolio V1"
         },
         description: {
-            fr: "Description du projet, en cours de rédaction...",
-            en: "Description of project, in progress..."
+            fr: "Réalisation de mon premier portfolio en React, à l'occasion de la soutenance du dernier projet de ma formation.",
+            en: "Creation of my first portfolio in React, during the defense of the last project of my training."
         },
         linkText: {
             fr: "Lien vers le repo",
@@ -31,20 +31,20 @@ const projects = [
     },
     {
         title: {
-            fr: "Projet 3 (Français)",
-            en: "Project 3 (English)"
+            fr: "Find my Car",
+            en: "Find my Car"
         },
         description: {
-            fr: "Description du projet 3, en cours de rédaction...",
-            en: "Description of project 3, in progress..."
+            fr: "Création d'une PWA permettant de sauvegarder une position GPS, et de les récupérer en ouvrant l'appli de navigation native pour y retourner.",
+            en: "Creation of a PWA allowing you to save a GPS position, and retrieve them by opening the native navigation app to return there."
         },
         linkText: {
             fr: "Lien vers le repo",
             en: "Link to repository"
         },
-        repoLink: "#"
+        repoLink: "https://github.com/AnthonyGodart/findmycar.git"
     },
-    {
+    /*{
         title: {
             fr: "Projet 4 (Français)",
             en: "Project 4 (English)"
@@ -58,5 +58,5 @@ const projects = [
             en: "Link to repository"
         },
         repoLink: "#"
-    },
+    },*/
 ];
